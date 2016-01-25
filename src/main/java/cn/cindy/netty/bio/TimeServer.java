e49c0cbddc3096fa8b23a6d9aa910767b602f3f1
caowenyan <284804814@qq.com>
2016-01-25 17:25:21 +0800

    传统的BIO编程模型

---
+++ src/main/java/cn/cindy/netty/bio/TimeServer.java
@@ -0,0 +1,39 @@
+package cn.cindy.netty.bio;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * 传统的BIO编程:同步阻塞I/O编程
+ *  使用客户端发送数据时,是telnet ip port,中间没有":"
+ */
+public class TimeServer {
+	
+	private static int port = 8080;
+	
+	public static void main(String[] args) {
+		ServerSocket server = null;
+		try {
+			server = new ServerSocket(port);
+			System.out.println("the time server is start in the port in : "+port);
+			Socket socket = null;
+			//通过循环来监听客户端连接,若没有客户端连接,则阻塞在accet方法上
+			while(true){
+				socket = server.accept();
+				new Thread(new TimeServerHandler(socket)).start();
+			}
+		} catch(Exception e){
+			e.printStackTrace();
+		} finally  {
+			if(server!=null)
+				System.out.println("the time server close");
+			try {
+				server.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			server = null;
+		}
+	}
+}
---
+++ src/main/java/cn/cindy/netty/bio/TimeServerHandler.java
@@ -0,0 +1,60 @@
+package cn.cindy.netty.bio;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class TimeServerHandler implements Runnable{
+	private Socket socket;
+	public TimeServerHandler(Socket socket) {
+		this.socket = socket;
+	}
+	
+	@Override
+	public void run() {
+		BufferedReader in = null;
+		PrintWriter out = null;
+		try {
+			in = new BufferedReader(new InputStreamReader(this.socket.getInputStream()));
+			out = new PrintWriter(this.socket.getOutputStream(),true);
+			String currentTime = null;
+			String body = null;
+			while(true){
+				body = in.readLine();
+				if(body==null){
+					break;
+				}
+				System.out.println("the time server receive data : "+body);
+				currentTime = "QUERY TIME".equalsIgnoreCase(body)? new Date(System.currentTimeMillis()).toString():"BAD ORDER";
+				out.println(currentTime);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				if(in!=null)
+					in.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			try {
+				if(out!=null)
+					out.close();
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			try {
+				if(socket!=null){
+					socket.close();
+					socket = null;
+				}
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+}
