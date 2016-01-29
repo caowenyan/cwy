0701393d96461de96d9648a8199c3992caa788a6
caowenyan <284804814@qq.com>
2016-01-29 19:05:09 +0800

    特定分隔符的编解码DelimiterBasedFrameDecoder

---
+++ src/main/java/cn/cindy/netty/pack/delimiter/TimeClientHandler.java
@@ -0,0 +1,41 @@
+package cn.cindy.netty.pack.delimiter;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TimeClientHandler extends ChannelInboundHandlerAdapter {
+	private int counter;
+	private final ByteBuf firstMessage;
+	private byte[] req;
+	
+	public TimeClientHandler(String message) {
+		message = message+"$_";
+		req = message.getBytes();
+        firstMessage = Unpooled.buffer(req.length);
+        firstMessage.writeBytes(req);
+	}
+	public void channelActive(ChannelHandlerContext ctx) throws Exception {
+		 //与服务端建立连接后
+		ByteBuf buf = null;
+		for (int i = 0; i < 100; i++) {
+			buf = Unpooled.buffer(req.length);
+			buf.writeBytes(req);
+			ctx.writeAndFlush(buf);
+		}
+	}
+
+	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //服务端返回消息后
+       String body = (String)msg;
+        System.out.println("Now is : " + body + " ; the counter is : " + ++counter);
+	}
+	
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+		// 释放资源
+		cause.printStackTrace();
+		ctx.close();
+	}
+
+}
