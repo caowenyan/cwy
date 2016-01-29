3906414ce4da8b6b8e39f3cf2303be5ab1d83412
caowenyan <284804814@qq.com>
2016-01-29 17:47:23 +0800

    解决error中黏包的问题,使用LineBasedFrameDecoder+StringDecoder

---
+++ src/main/java/cn/cindy/netty/pack/line/TimeServer.java
@@ -0,0 +1,52 @@
+package cn.cindy.netty.pack.line;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
+
+public class TimeServer {
+	
+	private static int port = 8080;
+	
+	public static void main(String[] args) {
+		new TimeServer().bind();
+	}
+	
+	public void bind(){
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(bossGroup, workerGroup)
+				.channel(NioServerSocketChannel.class)
+				.option(ChannelOption.SO_BACKLOG, 1024)
+				.childHandler(new ChildChannelHandler());
+			//绑定端口,同步等待成功
+			ChannelFuture channelFuture = serverBootstrap.bind(port).sync();
+			//等待服务器监听端口关闭
+			channelFuture.channel().closeFuture().sync();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}finally {
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+	
+	private class ChildChannelHandler extends ChannelInitializer<SocketChannel>{
+		@Override
+		protected void initChannel(SocketChannel ch) throws Exception {
+			ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+			ch.pipeline().addLast(new StringDecoder());
+			ch.pipeline().addLast(new TimeServerHandle());
+		}
+
+	}
+}
