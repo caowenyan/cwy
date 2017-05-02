d6b914c0148bd37460932d667c44cd8ab316a7fa
caowenyan <284804814@qq.com>
2017-05-02 17:26:03 +0800

    SpringContextHelper.java

---
+++ src/main/java/cn/cindy/util/SpringContextHelper.java
@@ -0,0 +1,20 @@
+package cn.cindy.util;
+
+/**
+*通过将bean配置到spring的容器中，可以获取bean
+*/
+public class SpringContextHelper implements ApplicationContextAware{
+	
+	private static ApplicationContext context = null;
+	
+	public static Object getBean(String name) {
+		return context == null ? null:context.getBean(name);
+	}
+	
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext)
+			throws BeansException {
+		context = applicationContext;
+		
+	}
+}
